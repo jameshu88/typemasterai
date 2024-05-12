07177c5a0fb6003fb2a0d9cd6afa2efd3d9c5516
tiffanylian <tiffanylian04@gmail.com>
2024-05-12 16:58:58 -0400

    Update HMTL/CSS to match mascot

--- public/sidePanelScript.js
+++ public/sidePanelScript.js
@@ -1,10 +1,10 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.type === 'sendText') {
         const wordCount = request.text.split(/\s+/).filter(Boolean).length;
-        document.getElementById('wordCountDisplay').textContent = `Words: ${wordCount}`;
+        document.getElementById('wordCountDisplay').textContent = `${wordCount}`;
         const charCount = request.text.length;
-        document.getElementById('charCountDisplay').textContent = `Characters: ${charCount}`;
+        document.getElementById('charCountDisplay').textContent = `${charCount}`;
         const charCountNoSpace = request.text.replace(/\s/g, "").length;
-        document.getElementById('charCountNoSpaceDisplay').textContent = `Characters (without spaces): ${charCountNoSpace}`;
+        document.getElementById('charCountNoSpaceDisplay').textContent = `${charCountNoSpace}`;
     }
 });
